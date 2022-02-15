4f7df9a8245188d5d125d6af120969750c2ec190
minjun7410 <minjun7410@gmail.com>
2022-02-15 23:36:21 +0900

    add user app

--- AccountBook/account_book/models.py
+++ AccountBook/account_book/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class AccountBook(models.Model):
+    email = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    present_account = models.IntegerField()
