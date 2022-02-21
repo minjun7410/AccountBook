2a9c32a27609bbdf3703f4909dfeb4075bd096e7
minjun7410 <minjun7410@gmail.com>
2022-02-21 20:09:53 +0900

    update account_book models (present_account -> day, month, year, price, description, location)

--- AccountBook/account_book/models.py
+++ AccountBook/account_book/models.py
@@ -3,6 +3,23 @@ from django.utils import timezone
 
 
 # Create your models here.
-class AccountBook(models.Model):
-    email = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    present_account = models.IntegerField()
+class AccountBook(models.Model):  # 트랜잭션을 저장하는 테이블
+    email = models.ForeignKey('user.User', on_delete=models.CASCADE)  # on_delete=models.CASCADE : 이 외래키가 지정하는 행이 지워지면 현재 행도 지워짐
+    price = models.IntegerField(null=True)
+    year = models.IntegerField(null=True)
+    month = models.IntegerField(null=True)
+    day = models.IntegerField(null=True)
+    description = models.CharField(max_length=200, default='')
+    location = models.CharField(max_length=50, default='')
+
+    def define(self, email, price, year, month, day, description, location):
+        self.email = email
+        self.price = price
+        self.year = year
+        self.month = month
+        self.day = day
+        self.description = description
+        self.location = location
+        return
+
+
