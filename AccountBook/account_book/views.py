3d37aa26910efc6da7c7c62c4a046cf08d766347
minjun7410 <minjun7410@gmail.com>
2022-02-21 20:11:58 +0900

    add account.html, update urls, views

--- AccountBook/account_book/views.py
+++ AccountBook/account_book/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import AccountBook
+import calendar
+import datetime
 
 # Create your views here.
+def account(request):
+    querysets = AccountBook.objects.filter(email=request.session['email'])
+    if request.method == 'GET':
+        current_year = datetime.date.year
+        current_month = datetime.date.month
+        print(querysets)
+        return render(request, 'account_book/account.html', {'username': request.session['username']})
+    else:
+        return render(request, 'account_book/account.html', {})
+
--- AccountBook/user/views.py
+++ AccountBook/user/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from django.contrib import messages
+from django.contrib.auth.hashers import check_password
 
 # Create your views here.
 def signup(request):
     if request.method == "POST" :
         if not(User.objects.filter(email=request.POST['email'])):
-            user = User.objects.create_user(email=request.POST['email'], username=request.POST['username'], password=request.POST['password'])
+            User.objects.create_user(email=request.POST['email'], username=request.POST['username'], password=request.POST['password'])
             return render(request, 'user/login.html', {})
         else:
             messages.info(request, 'Duplicated email. rewrite!')
@@ -16,6 +17,19 @@ def signup(request):
 
 def login(request):
     if request.method == "POST":
-        return render(request, 'user/login.html', {})
+        email = request.POST['email']
+        if not(User.objects.filter(email=email)):
+            messages.info(request, "No Email")
+            return render(request, 'user/login.html', {})
+        else:
+            user_queryset = User.objects.get(email=email)
+            if check_password(request.POST['password'], user_queryset.password):
+                request.session['username'] = user_queryset.username
+                request.session['email'] = user_queryset.email
+                request.session.modified = True
+                return redirect('/account')
+            else:
+                messages.info(request, "Password do not match")
+                return render(request, 'user/login.html', {})
     else:
         return render(request, 'user/login.html', {})
